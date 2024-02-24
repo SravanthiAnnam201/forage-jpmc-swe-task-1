a44645ab8dfdabb461ded824197424b3ce77c705
SravanthiAnnam201 <sravanthi201201@gmail.com>
2024-02-24 15:18:57 +0530

    Add earlier Python files

--- client_test.py
+++ client_test.py
@@ -7,6 +7,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
+    result = getDataPoint(quotes[0])
+    self.assertEqual(result, ('ABC', 120.48, 121.2, (120.48 + 121.2) / 2))
     """ ------------ Add the assertion below ------------ """
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
@@ -14,11 +16,19 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
+    result = getDataPoint(quotes[0])
+    self.assertEqual(result, ('ABC', 120.48, 119.2, (120.48 + 119.2) / 2))
     """ ------------ Add the assertion below ------------ """
-
-
-  """ ------------ Add more unit tests ------------ """
-
+  def test_getDataPoint_differentStockName(self):
+    quotes = [
+      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'XYZ'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+      # Call the function to get the result
+    result = getDataPoint(quotes[0])
+      # Assert that the result is as expected
+    self.assertEqual(result, ('XYZ', 120.48, 121.2, (120.48 + 121.2) / 2))
+    self.assertEqual(result[0], 'XYZ')
 
 
 if __name__ == '__main__':
